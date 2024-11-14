bfa5847d72244933ffa3f0751a2d74a075cf31b8
Ivan Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2024-11-13 21:50:14 -0500

    iniciando taller

--- hello-cdk/src/main/java/com/myorg/HelloCdkApp.java
+++ hello-cdk/src/main/java/com/myorg/HelloCdkApp.java
@@ -4,13 +4,18 @@ import software.amazon.awscdk.App;
 import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.StackProps;
 
-import java.util.Arrays;
-
 public class HelloCdkApp {
+    private static final String awsAccountId = System.getenv("awsAccountId");
+    private static final String awsRegion = System.getenv("awsRegion");
+
     public static void main(final String[] args) {
         App app = new App();
 
         new HelloCdkStack(app, "HelloCdkStack", StackProps.builder()
+                .env(Environment.builder()
+                        .account(awsAccountId)
+                        .region(awsRegion)
+                        .build())
                 // If you don't specify 'env', this stack will be environment-agnostic.
                 // Account/Region-dependent features and context lookups will not work,
                 // but a single synthesized template can be deployed anywhere.
@@ -26,12 +31,7 @@ public class HelloCdkApp {
 
                 // Uncomment the next block if you know exactly what Account and Region you
                 // want to deploy the stack to.
-                /*
-                .env(Environment.builder()
-                        .account("123456789012")
-                        .region("us-east-1")
-                        .build())
-                */
+
 
                 // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
                 .build());
